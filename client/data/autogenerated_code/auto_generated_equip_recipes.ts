3ed603920ed2e5f7bacf0e17f2c9cb937380bf6c
Lergassi <lergassi.dev@yandex.ru>
2023-02-24 11:04:38 +0500

    Автогенерация кода для импорта json в webpack.

---
+++ client/data/autogenerated_code/auto_generated_equip_recipes.ts
@@ -0,0 +1,2 @@
+import _auto_generated_equip_recipes_db_data from '../../../core/data/json/auto_generated_equip_recipes_24.02.2023_06_02_55.json';
+export const auto_generated_equip_recipes_db_data = _auto_generated_equip_recipes_db_data;
